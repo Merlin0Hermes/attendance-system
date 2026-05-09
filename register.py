2be564c0a8bdfbeaa1230a301122b135c6700d58
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2026-05-09 11:05:45 +0530

    add better error message and add deepface as requirement

--- register.py
+++ register.py
@@ -1,3 +1,4 @@
+from streamlit_authenticator.utilities.exceptions import RegisterError
 import time
 from storage import add_user
 from auth import save_config
@@ -6,11 +7,14 @@ import streamlit as st
 st.set_page_config(page_title="Register")
 st.session_state.authenticator.login(location="unrendered")
 
-email, username, name = st.session_state.authenticator.register_user(password_hint=False)
-if email:
-    st.success("User registered successfully")
-    save_config()
-    add_user(username) 
-    with st.spinner("Going to login page...", show_time=True):
-        time.sleep(3)
-    st.switch_page("login.py")
+try:
+    email, username, name = st.session_state.authenticator.register_user(password_hint=False)
+    if email:
+        st.success("User registered successfully")
+        save_config()
+        add_user(username) 
+        with st.spinner("Going to login page...", show_time=True):
+            time.sleep(3)
+        st.switch_page("login.py")
+except RegisterError as e:
+    st.error(e)
